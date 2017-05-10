979592a3012b54a8d8395acfbef130d2e1fa5b8b
marcos <marpernas@gmail.com>
2017-05-09 21:31:58 -0300

    projeto exchange ajustado

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ require './lib/exchange'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module OnebitcodeExchange
+module CurrencyConverter
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
